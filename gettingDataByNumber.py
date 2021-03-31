2f72ca7ac8f8394340080dfeb3a28bdb0a0474c8
denissv <volshebnik00@gmail.com>
2021-03-31 21:10:12 +0300

    clear

--- gettingDataByNumber.py
+++ gettingDataByNumber.py
@@ -1,7 +1,4 @@
 import requests
-import json
-
-#n = "AE0807OA"
 
 
 def parseNumber(number):
@@ -11,27 +8,29 @@ def parseNumber(number):
         r = requests.get(url, headers={"Accept": "application/json"})
         data = r.json()
 
+
         for item in data['operations']:
+            if item['isLast']:
 
-            if data['stolen']:
-                statusStolen = '❌ Да ❌'
-            else:
-                statusStolen = '✅ Нет ✅'
 
-            operation = (
-                'Производитель: ' + item['vendor'] + '\n'
-                'Модель: ' + item['model'] + '\n'
-                'Год: ' + str(item['modelYear']) + '\n'
-                'Приметы: ' + item['notes'] + '\n'
-                'Врозыске: ' + statusStolen + '\n'                              
-                'Операция: ' + item['operation'] + '\n'
-                'Дата: ' + str(item['regAt']) + '\n'                                  
-                'Адрес: ' + item['address']
-            )
+                if data['stolen']:
+                    statusStolen = '❌ Да ❌'
+                else:
+                    statusStolen = '✅ Нет ✅'
+
+                operation = (
+                    'Производитель: ' + item['vendor'] + '\n'
+                    'Модель: ' + item['model'] + '\n'
+                    'Год: ' + str(item['modelYear']) + '\n'
+                    'Приметы: ' + item['notes'] + '\n'
+                    'В розыске: ' + statusStolen + '\n'                              
+                    'Операция: ' + item['operation'] + '\n'
+                    'Дата: ' + str(item['regAt']) + '\n'                                  
+                    'Адрес: ' + item['address']
+                )
 
     except KeyError:
         operation = '⛔Номер не найден⛔'
 
     return operation
-
 
